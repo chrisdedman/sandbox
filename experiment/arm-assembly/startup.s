712bda17740d0a91cd6c8792bdc4e52f9da7f326
Chris <rolletchris1@gmail.com>
2024-02-24 12:37:59 -0800

    Startup file for QEMU simulation with Linker Directive file (commands to assembly, link and run the simu on the startup.o file).Y

--- experiment/arm-assembly/startup.s
+++ experiment/arm-assembly/startup.s
@@ -22,5 +22,11 @@ _exit:
     svc #0
 
 // clang --target=armv7m -fuse-ld=lld example.s -nostdlib
-// as -target armv7m  example.s -o example.o
 
+/* 
+ * Commands to run to assembly, link and run this sim
+ * as -target armv7m  startup.s -o startup.o
+ * /opt/homebrew/Cellar/llvm/17.0.6_1/bin/ld.lld -T test.ld startup.o -o test.elf  
+ * /opt/homebrew/Cellar/llvm/17.0.6_1/bin/llvm-objcopy -O binary test.elf test.bin
+ * qemu-system-arm -M versatilepb -m 128M -nographic -s -S -kernel test.bin
+ */
