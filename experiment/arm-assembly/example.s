30db291b11984e180e202fa9b12aeaa64da439ba
Chris <rolletchris1@gmail.com>
2024-02-23 23:16:53 -0800

    ARMv7-M assembly language to add two numbers together

--- experiment/arm-assembly/example.s
+++ experiment/arm-assembly/example.s
@@ -1,9 +1,26 @@
-// simple program that defines
-// a integer variable
-// and adds four to it
-
+// ARMv7-M assembly example
+// This example demonstrates how to use
+// the ARMv7-M assembly language to add 
+// two numbers and return the result.
 .global _start
-_start:
-    mov r0, #2
 
-//clang --target=armv7m -fuse-ld=lld example.s -nostdlib
+_start:
+    mov r1, #5
+    mov r2, #6
+    b mylabel
+
+result:
+    mov r0, r4
+    b _exit
+
+mylabel:
+    add r4, r1, r2
+    b result
+
+_exit:
+    mov r7, #0
+    svc #0
+
+// clang --target=armv7m -fuse-ld=lld example.s -nostdlib
+// as -target armv7m  example.s -o example.o
+
